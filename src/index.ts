413b53cce73e900e012a8b9f53e834707771b7f4
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-07-15 21:08:50 +0530

    WS Initialization

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//Event Handler 
+wss.on("connection",function(socket){
+    console.log("user connected")
+    setInterval(()=>{
+        socket.send(`Time is : ${new Date().toLocaleTimeString()}`);
+    },2000)
+
+    socket.on("message",(e)=>{
+        console.log("Message from client: ", e.toString());
+    })
+})
+
