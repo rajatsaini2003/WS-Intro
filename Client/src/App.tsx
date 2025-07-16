6e0b52f71d29b328c4870c707f6faf5064c235a9
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-07-17 00:22:21 +0530

    ping-pong client server ws connection

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [socket, setSocket] = useState();
+  const ref=useRef()
+  function sendMessage() {
+    if(!socket)return;
+    const message= ref.current.value;
+    //@ts-ignore
+    socket.send(message);
+  }
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+    
+    ws.onmessage = (event) => {
+      alert('Message from server: ' + event.data);
+    }
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input ref={ref} type='text' placeholder='Message...'></input>
+      <button onClick={sendMessage}>Send</button>
+    </div>
   )
 }
 
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -5,10 +5,6 @@ const wss = new WebSocketServer({ port: 8080 });
 //Event Handler 
 wss.on("connection",function(socket){
     console.log("user connected")
-    setInterval(()=>{
-        socket.send(`Time is : ${new Date().toLocaleTimeString()}`);
-    },2000)
-
     socket.on("message",(e)=>{
         if(e.toString() === 'ping'){
             socket.send('pong');
